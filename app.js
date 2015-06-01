8052f3b06c7aa053be2d318a3c8df4419aaebd09
anton-b117 <anton@maroonspear.com>
2015-06-01 17:32:45 -0400

    Setting up date and time info.

--- app.js
+++ app.js
@@ -6,6 +6,41 @@ angular.module('scheduleApp', ['firebase'])
 // create our main controller and get access to firebase
 .controller('mainController', function($scope, $firebase) {
 
-	// our application code will go here
+	// connect to firebase
+	var ref = new Firebase("https://progal.firebaseio.com/days");
+	var fb = $firebase(ref);
+
+	// function to set the default data
+	$scope.reset = function() {
+
+		fb.$set({
+			monday: {
+				name: 'Monday',
+				slots: {
+					0900: {
+						time: '9:00am',
+						booked: false
+					},
+					0110: {
+						time: false
+					}
+				}
+			},
+			tuesday: {
+				name: 'Tuesday',
+				slots: {
+					0900: {
+						time: '9:00am',
+						booked: false
+					},
+					0110: {
+						time: '11:00am',
+						booked: false
+					}
+				}
+			}
+		});
+	
+	};
 
 });
